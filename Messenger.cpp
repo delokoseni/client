d03d26fd717038e98923cc0ba8bbbc6feb00891b
Artur Belsh <107359196+delokoseni@users.noreply.github.com>
2024-05-07 23:15:20 +0600

    Наброски окна с чатами

--- Messenger.cpp
+++ Messenger.cpp
@@ -1,6 +1,37 @@
 #include "Messenger.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout> // Добавьте этот заголовочный файл для горизонтального макета
+#include <QWidget>
 
 Messenger::Messenger(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("Чаты");
-    resize(800, 800);
+    resize(800, 600);
+
+    // Основной виджет и вертикальный макет
+    QWidget *centralWidget = new QWidget(this);
+    QVBoxLayout *verticalLayout = new QVBoxLayout(centralWidget);
+    setCentralWidget(centralWidget);
+
+    // Горизонтальный макет для поиска
+    QHBoxLayout *searchLayout = new QHBoxLayout();
+    searchEdit = new QLineEdit();
+    searchEdit->setPlaceholderText("Поиск...");
+    searchLayout->addWidget(searchEdit);
+
+    // Создаем кнопку "Найти" и добавляем её в горизонтальный макет
+    QPushButton *searchButton = new QPushButton("Найти");
+    searchLayout->addWidget(searchButton);
+
+    // Добавляем горизонтальный макет в вертикальный макет
+    verticalLayout->addLayout(searchLayout);
+
+    // Настраиваем макет, чтобы кнопка "Выйти" была внизу
+    verticalLayout->addStretch();
+
+    // Добавляем кнопку "Выйти" в нижней части
+    exitButton = new QPushButton("Выйти");
+    verticalLayout->addWidget(exitButton);
+
+    // Подключаем кнопку "Выйти" к слоту close() окна
+    connect(exitButton, &QPushButton::clicked, this, &Messenger::close);
 }
--- Messenger.h
+++ Messenger.h
@@ -2,12 +2,18 @@
 #define MESSENGER_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
 
 class Messenger : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit Messenger(QWidget *parent = nullptr);
+
+private:
+    QLineEdit *searchEdit;
+    QPushButton *exitButton;
 };
 
 #endif // MESSENGER_H
